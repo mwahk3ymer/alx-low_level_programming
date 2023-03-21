e4536a6e9de184dc94912f9c486d815b6c0029d0
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-21 06:25:30 -0700

    my main putchar and readme

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,25 +7,25 @@
  */
 int main(void)
 {
-    char c = '_';
+char c = '_';
 
-    write(STDOUT_FILENO, &c, 1);
-    c = 'p';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'u';
-    write(STDOUT_FILENO, &c, 1);
-    c = 't';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'c';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'h';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'a';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'r';
-    write(STDOUT_FILENO, &c, 1);
-    c = '\n';
-    write(STDOUT_FILENO, &c, 1);
+write(STDOUT_FILENO, &c, 1);
+c = 'p';
+write(STDOUT_FILENO, &c, 1);
+c = 'u';
+write(STDOUT_FILENO, &c, 1);
+c = 't';
+write(STDOUT_FILENO, &c, 1);
+c = 'c';
+write(STDOUT_FILENO, &c, 1);
+c = 'h';
+write(STDOUT_FILENO, &c, 1);
+c = 'a';
+write(STDOUT_FILENO, &c, 1);
+c = 'r';
+write(STDOUT_FILENO, &c, 1);
+c = '\n';
+write(STDOUT_FILENO, &c, 1);
 
-    return (0);
+return (0);
 }
