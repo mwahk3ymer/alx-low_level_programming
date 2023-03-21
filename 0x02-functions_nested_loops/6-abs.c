e63fe4c8eec5c6f97e8b0382f0a702469f584d42
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-21 08:02:30 -0700

    absolute alue

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _abs - Computes the absolute value of an integer.
@@ -8,8 +8,8 @@
  */
 int _abs(int n)
 {
-if (n < 0)
-return (n * -1);
-else
+if (n >= 0)
 return (n);
+else
+return (-n);
 }
