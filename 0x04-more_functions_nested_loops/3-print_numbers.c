1d3a71b5e64180fec0f06acf019fc8f3e23d67f1
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-23 02:47:35 -0700

    prints nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+# include  "main.h"
+
+/**
+ * print_numbers(void) - prints nmbers from 0 to 10
+ * followed by a new line
+ */
+
+void print_numbers(void)
+{
+int i;
+char c;
+
+for (i = 0; i < 10; i++)
+{
+c = i + '0';
+putchar(c);
+}
+
+putchar('\n');
+}
