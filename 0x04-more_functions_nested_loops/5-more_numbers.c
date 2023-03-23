216375413be2d521bec58ddcc5cfa68472e7b9e2
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-23 07:38:08 -0700

    more nums

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-*more_numbers - print num
-*/
+ * more_numbers - print more numbers
+ */
 
 void more_numbers(void)
 {
@@ -14,7 +14,7 @@ for (j = 0; j <= 14; j++)
 {
 if (j >= 10)
 _putchar('1');
-_putchar((j % 10 + '0');
+_putchar (j % 10 + '0');
 }
 _putchar('\n');
 }
