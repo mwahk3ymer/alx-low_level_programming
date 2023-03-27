196657c6f73dff95e41c2e339b583562b04dddca
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-27 10:55:01 -0700

    further commits

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,23 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
+ * main - generates random passwords for 101-crackme
+ * Return: zero
  */
-char *_strcpy(char *dest, char *src)
+int main(void)
 {
-	int l = 0;
-	int x = 0;
+	int sum;
+	char c;
 
-	while (*(src + l) != '\0')
+	srand(time(NULL));
+	while (sum <= 2645)
 	{
-		l++;
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
 	}
-	for ( ; x < l ; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
-	return (dest);
+	putchar(2772 - sum);
+	return (0);
 }
