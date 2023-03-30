11fab4d13364cf2ccf8691aa6d2fd5e1aef3c436
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-30 01:40:22 -0700

    corrections on leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,46 +1,24 @@
+#include "main.h"
 /**
- * leet - Encodes a string into 1337.
- * @str: The string to encode.
- * Return: The encoded string, or NULL if allocation fails.
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
  */
-char *leet(char *str)
+char *leet(char *n)
 {
-	if (!str)
-		return (NULL);
-
-	char *result = malloc(sizeof(char) * (strlen(str) + 1));
-
-	if (!result)
-		return (NULL);
-
-	char *leet_chars = "aAeEoOtTlL";
-	char *leet_codes = "4433007711";
-
 	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for (i = 0, j = 0; str[i] != '\0'; i++, j++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		char *pos = strchr(leet_chars, str[i]);
-
-		if (pos != NULL)
-			result[j] = leet_codes[pos - leet_chars];
-		else
-			result[j] = str[i];
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
 	}
-	result[j] = '\0';
-
-	while (*str != '\0')
-	{
-		char *encoded_line = leet(str);
-
-		write(STDOUT_FILENO, encoded_line, strlen(encoded_line));
-
-		free(encoded_line);
-
-		str = strchr(str, '\n');
-		if (str != NULL)
-			str++;
-	}
-
-	return (result);
+	return (n);
 }
