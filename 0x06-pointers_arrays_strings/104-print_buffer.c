5094f161099db6e2cb8ef9f0d72a9333646a4b0a
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-30 01:53:37 -0700

    advanced commits

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,41 +10,41 @@
 
 void print_buffer(char *b, int size)
 {
-  int o, j, i;
+	int o, j, i;
 
-  o = 0;
+	o = 0;
 
-  if (size <= 0)
-    {
-      printf("\n");
-      return;
-    }
-  while (o < size)
-    {
-      j = size - o < 10 ? size - o : 10;
-      printf("%08x: ", o);
-      for (i = 0; i < 10; i++)
+	if (size <= 0)
 	{
-	  if (i < j)
-	    printf("%02x", *(b + o + i));
-	  else
-	    printf("  ");
-	  if (i % 2)
-	    {
-	      printf(" ");
-	    }
+		printf("\n");
+		return;
 	}
-      for (i = 0; i < j; i++)
+	while (o < size)
 	{
-	  int c = *(b + o + i);
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
 
-	  if (c < 32 || c > 132)
-	    {
-	      c = '.';
-	    }
-	  printf("%c", c);
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
 	}
-      printf("\n");
-      o += 10;
-    }
 }
