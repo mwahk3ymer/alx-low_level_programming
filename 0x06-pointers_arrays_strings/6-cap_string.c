3e2262470ddbd308c112e607ae99332d05d2f98b
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-30 00:17:24 -0700

    capitalise string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,31 @@
 #include<stddef.h>
 /**
- * cap_string - Capitalizes all words in a given string
- * @str: the input string to be capitalized
+ * cap_string - capitalizes all words in a string
+ * @str: the string to capitalize
  *
- * Return: a pointer to the capitalized string
+ * Return: the capitalized string
  */
 char *cap_string(char *str)
 {
-int i;
+    int i;
 
-if (str == NULL)
-return (NULL);
+    if (str[0] >= 'a' && str[0] <= 'z')
+    {
+        str[0] = str[0] - 32;
+    }
 
-for (i = 0; str[i] != '\0'; i++)
-{
-if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
-str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
-str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
-str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-str[i - 1] == '{' || str[i - 1] == '}')
-{
-str[i] = toupper(str[i]);
-}
-}
-return (str);
+    for (i = 1; str[i] != '\0'; i++)
+    {
+        if ((str[i] >= 'a' && str[i] <= 'z') &&
+            (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+             str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+             str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+             str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+             str[i - 1] == '}'))
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+
+    return (str);
 }
