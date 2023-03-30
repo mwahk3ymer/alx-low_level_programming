1c614c9930310ee5448b2e9037df5f74b3152abc
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-29 23:47:43 -0700

    capitalize a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,29 @@
 /**
  * cap_string - Capitalizes all words of a string.
- * @str: The input string.
+ * @s: The string to capitalize.
  *
- * Return: A pointer to the modified string.
+ * Return: The capitalized string.
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 int i;
 
- /* Capitalize the first letter */
-if (str[0] != '\0' && islower(str[0]))
-str[0] = toupper(str[0]);
+if (s == NULL)
+return (NULL);
 
- /* Capitalize subsequent letters after separators */
-for (i = 1; str[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-if (is_separator(str[i - 1]) && islower(str[i]))
-str[i] = toupper(str[i]);
+if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' ||
+s[i - 1] == '\n' || s[i - 1] == ',' ||
+s[i - 1] == ';' || s[i - 1] == '.' ||
+s[i - 1] == '!' || s[i - 1] == '?' ||
+s[i - 1] == '"' || s[i - 1] == '(' ||
+s[i - 1] == ')' || s[i - 1] == '{' ||
+s[i - 1] == '}')
+{
+s[i] = s[i] - 32;
+}
 }
 
-return (str);
-}
-
-/**
- * is_separator - Determines if a character is a word separator.
- * @c: The input character.
- *
- * Return: 1 if c is a separator, 0 otherwise.
- */
-int is_separator(char c)
-{
-return (isspace(c) || c == ',' || c == ';' || c == '.' ||
-c == '!' || c == '?' || c == '"' || c == '(' ||
-c == ')' || c == '{' || c == '}');
+return (s);
 }
