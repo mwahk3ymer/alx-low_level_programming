a754c012cf05be852a6d0be68c7de9fecee9d557
mwahk3ymer <audreymwakima99@gmail.com>
2023-03-30 00:20:24 -0700

    capitalise string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,25 +7,25 @@
  */
 char *cap_string(char *str)
 {
-    int i;
+int i;
 
-    if (str[0] >= 'a' && str[0] <= 'z')
-    {
-        str[0] = str[0] - 32;
-    }
+if (str[0] >= 'a' && str[0] <= 'z')
+{
+str[0] = str[0] - 32;
+}
 
-    for (i = 1; str[i] != '\0'; i++)
-    {
-        if ((str[i] >= 'a' && str[i] <= 'z') &&
-            (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-             str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-             str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
-             str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
-             str[i - 1] == '}'))
-        {
-            str[i] = str[i] - 32;
-        }
-    }
+for (i = 1; str[i] != '\0'; i++)
+{
+if ((str[i] >= 'a' && str[i] <= 'z') &&
+(str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+str[i - 1] == '}'))
+{
+str[i] = str[i] - 32;
+}
+}
 
-    return (str);
+return (str);
 }
