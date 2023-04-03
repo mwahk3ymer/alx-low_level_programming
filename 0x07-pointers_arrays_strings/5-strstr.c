bd894792db1a46917357741b5a318f12a2fc4b85
mwahk3ymer <audreymwakima99@gmail.com>
2023-04-03 05:25:39 -0700

    correct locates on a string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,21 +9,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-        while (*haystack != '\0') {
-                char *p1 = haystack;
-                char *p2 = needle;
+while (*haystack != '\0')
+{
+char *p1 = haystack;
+char *p2 = needle;
 
-                while (*p1 == *p2 && *p2 != '\0') {
-                        ++p1;
-                        ++p2;
-                }
+while (*p1 == *p2 && *p2 != '\0')
+{
+++p1;
+++p2;
+}
 
-                if (*p2 == '\0') {
-                        return haystack;
-                }
+if (*p2 == '\0')
+{
+return (haystack);
+}
 
-                ++haystack;
-        }
+++haystack;
+}
 
-        return NULL;
+return (NULL);
 }
