a226a16da099c0c5675e2116aeb45fe579a098e2
mwahk3ymer <audreymwakima99@gmail.com>
2023-04-05 01:21:07 -0700

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: pointer to the string to print
@@ -9,10 +10,10 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 
-	_putchar(*s);
+	putchar(*s);
 	_puts_recursion(s + 1);
 }
 
 else
-_putchar('\n');
+putchar('\n');
 }
