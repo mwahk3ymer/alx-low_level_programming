0e9186adaffae5083e76bb76d58567c253b44609
mwahk3ymer <audreymwakima99@gmail.com>
2023-04-05 03:27:54 -0700

    correction advanced

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,56 +1,49 @@
 /**
- * _strlen - returns the length of a string
- * @s: the string to get the length of
+ * _strlen - Returns the length of a string.
+ * @s: The string to measure.
  *
- * Return: the length of the string
+ * Return: The length of the string.
  */
 int _strlen(char *s)
 {
-int len = 0;
-
-while (*s != '\0')
-{
-len++;
-s++;
-}
-
-return (len);
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen(s + 1) + 1);
 }
 
 /**
- * is_palindrome_helper - checks if a string is a palindrome
- * @s: the string to check
- * @start: the starting index of the string
- * @end: the ending index of the string
+ * is_palindrome_helper - Compares the first and last characters of a string,
+ *                        then recursively calls itself with the middle of the
+ *                        string until the entire string has been compared.
+ * @s: The string to check.
+ * @start: The index of the first character to check.
+ * @end: The index of the last character to check.
  *
- * Return: 1 if the string is a palindrome, 0 otherwise
+ * Return: 1 if the string is a palindrome, 0 otherwise.
  */
 int is_palindrome_helper(char *s, int start, int end)
 {
-/* Base case: string has 0 or 1 characters */
-if (start >= end)
-return (1);
-
-
-if (s[start] == s[end])
-return (is_palindrome_helper(s, start + 1, end - 1));
-return (0);
+	if (start >= end)
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
+ * is_palindrome - Determines if a string is a palindrome.
+ * @s: The string to check.
  *
- * Return: 1 if the string is a palindrome, 0 otherwise
+ * Return: 1 if the string is a palindrome, 0 otherwise.
  */
 int is_palindrome(char *s)
 {
 int len = _strlen(s);
 
-/* Empty string is a palindrome */
 if (len == 0)
 return (1);
-
-/* Call helper function with start and end indices */
+else
 return (is_palindrome_helper(s, 0, len - 1));
 }
