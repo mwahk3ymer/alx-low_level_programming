50eeda010d16cbb3a2834f264e8b9be1a1a56d75
mwahk3ymer <audreymwakima99@gmail.com>
2023-04-11 03:29:57 -0700

    initial commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *array;
+unsigned int i;
 if (size == 0)
 return (NULL);
 
@@ -19,7 +20,7 @@ array = malloc(sizeof(*array) * size);
 if (array == NULL)
 return (NULL);
 
-for (unsigned int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 array[i] = c;
 
 return (array);
