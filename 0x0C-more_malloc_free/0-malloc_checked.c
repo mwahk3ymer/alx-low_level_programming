8d2d8934dd9df1e86b7cde4b390d5bbc37fbb790
mwahk3ymer <audreymwakima99@gmail.com>
2023-04-13 03:05:05 -0700

    fuction allocates mem using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+# include <stdlib.h>
+
+/**
+ *mallocc_checked - allocaates mem
+ *@b: the num of bytes to all
+ *
+ * Return: a pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
