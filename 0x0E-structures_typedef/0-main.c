1a618d7e13557bdf76d1dfdab378c80cfaa35a1e
mwahk3ymer <audreymwakima99@gmail.com>
2023-04-17 05:06:46 -0700

    trial

---
+++ 0x0E-structures_typedef/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog
+
+    dog.name = "Poppy";
+    dog.age = 3.5;
+    dog.owner = "Bob";
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifdef DOG_H
+
+#define DOG_H
+
+
+#include <stdlib.h>
+
+/**
+ * struct dog -rep dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of onwer
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
