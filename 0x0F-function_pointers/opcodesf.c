62c0bd1fed4b4718c5b9fe03fe70f818c83a657e
mwahk3ymer <audreymwakima99@gmail.com>
2023-04-19 09:22:27 -0700

    correct on advance

---
+++ 0x0F-function_pointers/opcodesf.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_opcodes - prints opcodes
+ * @n: num of bytes
+ */
+void print_opcodes(int n)
+{
+	unsigned char *opcode = (unsigned char *) &print_opcodes;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *(opcode +  i));
+	}
+	printf("\n");
+}
+
+/**
+ * main - entry
+ * @argc: num of arg
+ * @argv: array of arg
+ * Return: 0 on succes
+ */
+
+int main(int argc, char **argv)
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+
+if (n < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+print_opcodes(n);
+
+return (0);
+}
