8e1b4395a249dc77dde36687e430c9c85e8d95f3
mwahk3ymer <audreymwakima99@gmail.com>
2023-04-27 03:38:38 -0700

    coorrection on a function that prints all the elements of a list_t list2.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t t = 0;
+        size_t t = 0;
 
-	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+        while (h)
+        {
+                if (!h->str)
+                        printf("[0] (nil)\n");
+                else
+                        printf("[%u] %s\n", h->len, h->str);
 
-		h = h->next;
+                h = h->next;
 
-		t++;
-	}
+                t++;
+        }
 
-	return (t);
+        return (t);
 }
