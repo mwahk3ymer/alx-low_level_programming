342718c286f1e3cec76944f8210f7726d56bf9a3
mwahk3ymer <audreymwakima99@gmail.com>
2023-04-27 02:54:06 -0700

    correction on  a function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -16,6 +17,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
@@ -24,6 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->len = strlen(str);
 	new_node->next = *head;
+
 	*head = new_node;
 
 	return (new_node);
