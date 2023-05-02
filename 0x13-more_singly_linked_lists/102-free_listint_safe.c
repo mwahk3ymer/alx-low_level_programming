be4ed46642020cbf00701ef414520aaf2e825b97
mwahk3ymer <audreymwakima99@gmail.com>
2023-05-02 05:56:43 -0700

    frees listint_t list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,22 +11,26 @@ size_t free_listint_safe(listint_t **head)
 {
 	size_t size = 0;
 	listint_t *current, *tmp;
+	int diff;
 
 	if (!head || !*head)
 		return (size);
 	current = *head;
 	while (current)
 	{
-		size++;
-		if (current < current->next)
+		diff = current - (current)->next;
+		if (diff > 0)
 		{
 			tmp = current->next;
 			free(current);
 			current = tmp;
+			size++;
 		}
 		else
 		{
 			free(current);
+			current = NULL;
+			size++;
 			break;
 		}
 	}
