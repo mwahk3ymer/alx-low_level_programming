acc196117f30211b622bc5b786248c4be9a1f8e2
Mwahk3ymer <audreymwakima99@gmail.com>
2023-09-08 00:19:05 -0700

    table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,14 +10,13 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table_created;
+	hash_table_t *table_created;
 	hash_node_t **array;
 	unsigned long int i;
 
 	/* allocate space for the hash table */
-	hash_table_created = malloc(sizeof(hash_table_t));
-
-	if (hash_table_created == NULL)
+	table_created = malloc(sizeof(hash_table_t));
+	if (table_created == NULL)
 		return (NULL);
 
 	array = malloc(sizeof(hash_node_t *) * size);
@@ -27,8 +26,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	for (i = 0; i < size; i++)
 		array[i] = NULL;
 
-	hash_table_created->array = array;
-	hash_table_created->size = size;
+	table_created->array = array;
+	table_created->size = size;
 
-	return (hash_table_created);
+	return (table_created);
 }
