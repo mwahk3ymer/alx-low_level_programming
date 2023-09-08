155975c57eeee896b80d6e623982a21046dfe071
Mwahk3ymer <audreymwakima99@gmail.com>
2023-09-07 23:33:20 -0700

    4-hash

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,20 +10,20 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-    unsigned long int index = key_index((const unsigned char *)key, ht->size);
-    hash_node_t *current = ht->array[index];
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *current = ht->array[index];
 
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-        {
-            return (current->value);
-        }
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
